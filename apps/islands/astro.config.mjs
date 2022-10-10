5a82f9fb842048b33ec73d2792ac7ee8ca97d29d
lfberge <leiv.fredrik.berge@rikstv.no>
2022-10-10 08:35:42 +0200

    fix: astro deploy

--- apps/islands/astro.config.mjs
+++ apps/islands/astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   integrations: [preact()],
-  output: "server",
-  adapter: vercel(),
 });
